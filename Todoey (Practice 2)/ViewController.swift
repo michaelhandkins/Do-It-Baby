c8bb0dc6c51bf8520302475f12075a154de8fbcc
Michael Handkins <michaelhandkins@gmail.com>
2020-09-07 20:06:07 -0500

    Initial commit

---
+++ Todoey (Practice 2)/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey (Practice 2)
+//
+//  Created by Michael Handkins on 9/7/20.
+//  Copyright © 2020 Michael Handkins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
